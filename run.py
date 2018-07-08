d0686cda16d39c3b7f6ef98ec3e98b98b32074d0
William Roberts <weroberts@olivet.edu>
2018-07-07 23:11:24 -0700

    Add ability to update menubar

--- run.py
+++ run.py
@@ -1,14 +1,33 @@
-from tkinter import Tk, Toplevel
+from tkinter import Tk, Toplevel, Menu
 from config import Config
-from tshirtlogo import Application
+from tshirtlogo import MainCanvas
 from settings import Settings
 
+class MainWindow(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Computer Sciece Department Graphic")
+        self.menubar = Menu(self)
+
+    def addMenuCascade(self, label, commands):
+        menu = Menu(self.menubar, tearoff=False)
+        self.menubar.add_cascade(label=label, menu=menu)
+        for key, value in commands.items():
+            print(key)
+            menu.add_command(label=key, command=value)
+        self.__refreshMenuBar()
+
+    def setTitle(title):
+        self.title(title)
+
+    def __refreshMenuBar(self):
+        self.config(menu=self.menubar)
+
 if __name__ == '__main__':
-    root = Tk()
-    root.title("Computer Sciece Department Graphic")
+    mainWindow = MainWindow()
     settings = Config(bgColor="#005500", wireColor="#C5A953", hasStartNode=False, hasEndNode=False, hasStartModule=False, hasEndModule=False, isContinuous=False)
-    app = Application(settings=settings, master=root)
-    settingsWindowConfig = Toplevel(root)
+    canvas = MainCanvas(settings=settings)
+    settingsWindowConfig = Toplevel(mainWindow)
     settingsWindowConfig.title("Settings")
     Settings(settings=settings, master=settingsWindowConfig)
-    root.mainloop()
+    mainWindow.mainloop()
