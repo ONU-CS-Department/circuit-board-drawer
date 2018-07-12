6a7def5f3f7e0e60817b31adfd4932654a6c1cf6
William Roberts <weroberts@olivet.edu>
2018-07-11 22:30:21 -0700

    Modularize settings view
    Modularize settings.py

--- settings.py
+++ settings.py
@@ -1,71 +1,52 @@
-import tkinter as tk
-from tkinter import Button, Label, Entry, LEFT, StringVar, Frame, X, Checkbutton, HORIZONTAL, Menu, filedialog
-from tkinter.colorchooser import *
+from tkinter import Button, Label, Entry, LEFT, StringVar, Frame, X, Checkbutton, HORIZONTAL, Menu, filedialog, colorchooser
 
-class Settings(tk.Frame):
+class Settings(Frame):
     def __init__(self, settings=None, master=None):
         super().__init__(master)
         self.grid(row=0, columnspan=4)
-        Label(self, text = 'Background color:').grid(row = 0, column = 1)
         self.settings = settings
-        self.bgColorField = StringVar(self, value=self.settings.get("bgColor"))
-        self.updatebgColor = self.register(self.backgroundEntryChanged)
-        self.backgroundEntry = Entry(self, textvariable=self.bgColorField, width=15, validate='all', validatecommand=(self.updatebgColor, '%d', '%S'))
-        self.backgroundEntry.grid(row = 0, column = 3)
-        self.bgColorButton = Button(self, text='     ', command=self.setBGColor, bg=self.settings.get("bgColor"))
-        self.bgColorButton.grid(row = 0, column = 4)
-
-        Label(self, text = 'Draw color:').grid(row = 1, column = 1)
-        self.wireColorField = StringVar(self, value=self.settings.get("wireColor"))
-        self.updateWireColor = self.register(self.wireEntryChanged)
-        self.wireEntry = Entry(self, textvariable=self.wireColorField, width=15, validate='all', validatecommand=(self.updateWireColor, '%d', '%S'))
-        self.wireEntry.grid(row = 1, column = 3)
-        self.wireColorButton = Button(self, text='     ', command=self.setWireColor, bg=self.settings.get("wireColor"))
-        self.wireColorButton.grid(row = 1, column = 4)
+        
+        self.addColorPicker(0, 'Background color:', "bgColor")
+        self.addColorPicker(1, 'Draw color:', "wireColor")
 
         self.addCheckBox(2, "hasStartNode")
         self.addCheckBox(3, "hasEndNode")
         self.addCheckBox(4, "hasStartModule")
         self.addCheckBox(5, "hasEndModule")
         self.addCheckBox(6, "isContinuous")
-        
+
+    def addColorPicker(self, row, fieldLabel, settingValue):
+        Label(self, text=fieldLabel).grid(row=row, column=1)
+        colorField = StringVar(self, value=self.settings.get(settingValue))
+        button = Button(self, text='     ', bg=self.settings.get(settingValue))
+        button.configure(command=lambda: self.setButtonColor(row, settingValue, button, colorField))
+        button.grid(row=row, column=4)
+        entry = Entry(self, textvariable=colorField, width=15, validate='all')
+        entry.bind("<KeyPress>", lambda event, settingValue=settingValue, button=button: self.onKeyPress(settingValue, event, button))
+        entry.bind('<Control-v>', lambda e: 'break') #disable paste
+        entry.grid(row=row, column=3)
+
+    def onKeyPress(self, settingValue, event, button):
+        potentialChar = ""
+        if ((len(event.char) > 0) and (ord(event.char) < 128) and (ord(event.char) != 8)):
+            potentialChar = event.char
+        self.settings.set(settingValue, event.widget.get() + potentialChar)
+        try:
+            button.configure(bg=self.settings.get(settingValue))
+        finally:
+            return True
+
+    def setButtonColor(self, row, settingValue, button, field):
+        color = colorchooser.askcolor(self.settings.get(settingValue))[1]
+        if color:
+            field.set(color)
+            self.settings.set(settingValue, color)
+            button.configure(bg=self.settings.get(settingValue))
+
     def addCheckBox(self, row, value):
         Label(self, text = value.replace("_", " ").title() + ":").grid(row = row, column = 1)
         Checkbutton(self, command=lambda: self.toggleProperty(value)).grid(row=row, column=3)
 
     def toggleProperty(self, value):
         self.settings.set(value, not self.settings.get(value))
-
-    def backgroundEntryChanged(self, isInsertion, text):
-        if isInsertion == "1":
-            self.settings.set("bgColor", self.settings.get("bgColor") + text)
-        elif isInsertion == "0":
-            self.settings.set("bgColor", self.settings.get("bgColor")[:(-1 * len(text))])
-        try:
-            self.bgColorButton = Button(self, text='     ', command=self.setBGColor, bg=self.settings.get("bgColor")).grid(row = 0, column = 4)
-        finally:
-            return True
-
-    def wireEntryChanged(self, isInsertion, text):
-        if isInsertion == "1":
-            self.settings.set("wireColor", self.settings.get("wireColor") + text)
-        elif isInsertion == "0":
-            self.settings.set("wireColor", self.settings.get("wireColor")[:(-1 * len(text))])
-        try:
-            self.wireColorButton = Button(self, text='     ', command=self.setWireColor, bg=self.settings.get("wireColor")).grid(row = 1, column = 4)
-        finally:
-            return True
-
-    def setBGColor(self):
-        color = askcolor(self.settings.get("bgColor"))[1]
-        if color:
-            self.bgColorField.set(color)
-            self.settings.set("bgColor", color)
-            self.bgColorButton = Button(self, text='     ', command=self.setBGColor, bg=self.settings.get("bgColor")).grid(row = 0, column = 4)
-
-    def setWireColor(self):
-        color = askcolor(self.settings.get("wireColor"))[1]
-        if color:
-            self.wireColorField.set(color)
-            self.settings.set("wireColor", color)
-            self.wireColorButton = Button(self, text='     ', command=self.setWireColor, bg=self.settings.get("wireColor")).grid(row = 1, column = 4)
+            
